3c0883fbcec66ef8e53451920ce01cbfea11b5e9
Sazib204061 <mdsaziburrahman440@gmail.com>
2025-06-24 21:12:59 +0600

    update annotation.

--- MomotarJhuri.Domain/Entities/User.cs
+++ MomotarJhuri.Domain/Entities/User.cs
@@ -10,9 +10,9 @@ namespace MomotarJhuri.Domain.Entities
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
         public string? PhoneNumber { get; set; }
         public UserRole Role { get; set; }
         public string? Address { get; set; }
